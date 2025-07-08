4bdc9e886cae634822e8f450501615731cb5a6e5
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 23:41:01 +0500

    Add components\Features.js

---
+++ components\Features.js
@@ -0,0 +1,14 @@
+function Features() {
+  return (
+    <section>
+      <h2>Our Features</h2>
+      <ul>
+        <li>Fast Shipping</li>
+        <li>Secure Payment</li>
+        <li>Easy Returns</li>
+      </ul>
+    </section>
+  )
+}
+
+export default Features
