5d8471b6d3737eafb662247d966469069d7c85fd
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 23:41:07 +0500

    Add components\Hero.js

---
+++ components\Hero.js
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <section>
+      <h1>Welcome to our Ecommerce Website</h1>
+      <p>Explore our products and start shopping today!</p>
+      <button>Shop Now</button>
+    </section>
+  )
+}
+
+export default Hero
